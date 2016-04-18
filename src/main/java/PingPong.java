0766d1eff7daad154061009d38bf270cd971d022
Blake Bless and Luca Quatela <blake+luca@>
2016-04-18 16:50:38 -0700

    add divisiblity.

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -10,7 +10,15 @@ public class PingPong{
     public static ArrayList<Object> PingPongMethod(Integer number){
       ArrayList<Object> numArray = new ArrayList<Object>();
       for (Integer index = 1; number >= index; index++ ){
-        numArray.add(index);
+        if (index % 15 == 0){
+          numArray.add("PingPong");
+        } else if (index % 5 == 0){
+          numArray.add("Pong");
+        } else if (index % 3 == 0){
+            numArray.add("Ping");
+        } else {
+          numArray.add(index);
+        }
         System.out.println(numArray);
       }
       return numArray;
